1b1716aa1c026adf6bff46299dbac3e874604af6
n.altani <e.altani@apt.gr>
2025-09-08 17:41:54 +0300

    add type on service

--- src/types/api-football.ts
+++ src/types/api-football.ts
@@ -71,3 +71,76 @@ export type PlayerProfileItem = {
     photo: String,
     birth: any
 }
+
+
+
+export type FixtureServiceItem = {
+    fixture: {
+        id: number,
+        referee: string,
+        timezone: string,
+        data: string,
+        timestamp: number,
+        periods: {
+            first: number,
+            second: number
+        },
+        venue: {
+            id: number | null,
+            name: string,
+            city: string
+        },
+        status: {
+            long: string,
+            short: string,
+            elapsed: number,
+            extra: string | null
+        }
+    },
+    league: {
+        id: number,
+        name: string,
+        country: string,
+        logo: string,
+        flag: string,
+        season: number,
+        round: string,
+        standings: boolean
+    },
+    teams: {
+        home: {
+            id: number,
+            name: string,
+            logo: string,
+            winner: boolean
+        },
+        away: {
+            id: number,
+            name: string,
+            logo: string,
+            winner: boolean
+        }
+    },
+    goals: {
+        home: number | null,
+        away: number | null
+    },
+    score: {
+        halftime: {
+            home: number | null,
+            away: number | null
+        },
+        fulltime: {
+            home: number | null,
+            away: number | null
+        },
+        extratime: {
+            home: number | null,
+            away: number | null
+        },
+        penalty: {
+            home: number | null,
+            away: number | null
+        }
+    }
+}

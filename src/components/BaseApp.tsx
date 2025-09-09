c5e585644b961addb8f6d02a1c94247f6023972f
n.altani <e.altani@apt.gr>
2025-09-09 15:36:17 +0300

    add style to main appbar

--- src/components/BaseApp.tsx
+++ src/components/BaseApp.tsx
@@ -3,7 +3,7 @@
 import AppBar from "@mui/material/AppBar"
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-
+import Grid from "@mui/material/Grid"
 
 
 export default function AppBarHeader({
@@ -12,12 +12,14 @@ export default function AppBarHeader({
 
     }) {
     return (
-        <AppBar position="fixed" sx={{ zIndex: (t) => t.zIndex.drawer + 1 }}>
-            <Toolbar >
-                <Typography variant="h6" noWrap >
-                    Football
+        <AppBar position="fixed" color="primary" sx={{borderBottom:'1px solid #1E2229'}}>
+            <Toolbar sx={{ display: 'flex', justifyContent: 'center', width: '100%' }}>
+                <Typography variant="h4" noWrap >
+                    <Grid >
+                        <span style={{fontWeight:"700"}}>Football app</span>
+                    </Grid>
                 </Typography>
             </Toolbar>
-        </AppBar>
+        </AppBar >
     )
 }
--- src/theme.ts
+++ src/theme.ts
@@ -6,8 +6,8 @@ import { createTheme } from "@mui/material/styles";
 const theme = createTheme({
     palette: {
         mode: 'light',
-        primary: { main: '#1976d2' },
-        secondary: { main: '#9c27b0' },
+        primary: { main: '#0F1214' },
+        secondary: { main: '#15181A' },
     },
     typography: {
         fontFamily: 'var(--font-roboto), Roboto, system-ui, -apple-system, sans-serif',

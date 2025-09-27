78b4aa05a51c0f217d130151ad26d6605301ec4b
Altani Niki <35735676+AltaniNiki@users.noreply.github.com>
2025-09-27 21:47:54 +0300

    Update apiClient.ts

--- src/lib/http/apiClient.ts
+++ src/lib/http/apiClient.ts
@@ -6,7 +6,7 @@ export const apiClient = axios.create({
     baseURL: APISPORTS_BASE,
     timeout: 15_000,
     headers: {
-        'x-apisports-key': 04fee3c827a5d989331c5d792674fd2f, // ή x-rapidapi-key αν χρησιμοποιείς RapidAPI
+        'x-apisports-key': '04fee3c827a5d989331c5d792674fd2f', // ή x-rapidapi-key αν χρησιμοποιείς RapidAPI
     },
 });
 
